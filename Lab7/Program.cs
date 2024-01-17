ce5e0480f7dab3b91254751016816a5a79f1e986
Lau <laurentiucozma12@gmail.com>
2024-01-17 11:33:40 +0200

    lab10

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -7,7 +7,7 @@ using Nume_Pren_Lab2.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container. lab10
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
